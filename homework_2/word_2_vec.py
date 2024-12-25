6a3a39f2c8035739685bbd847dbd774ae736d54f
Ran Wright <ranman37@gmail.com>
2024-12-25 15:00:16 +0200

    issue17

--- homework_2/word_2_vec.py
+++ homework_2/word_2_vec.py
@@ -1,17 +1,28 @@
 import numpy as np
 from gensim.models import Word2Vec
+from nltk.tokenize import word_tokenize
 
 
 def train_skipgram_model(corpus, vector_size, window, min_count):
     model = Word2Vec(
-        corpus, vector_size=vector_size, window=window, min_count=min_count, sg=1
+        corpus,
+        vector_size=vector_size,
+        window=window,
+        min_count=min_count,
+        sg=1,
+        alpha=0.001,
     )
     return model
 
 
 def train_cbow_model(corpus, vector_size, window, min_count):
     model = Word2Vec(
-        corpus, vector_size=vector_size, window=window, min_count=min_count, sg=0
+        corpus,
+        vector_size=vector_size,
+        window=window,
+        min_count=min_count,
+        sg=0,
+        alpha=0.001,
     )
     return model
 
@@ -19,8 +30,8 @@ def train_cbow_model(corpus, vector_size, window, min_count):
 # Function to read sentences from a file
 def read_sentences(file_path):
     with open(file_path, "r") as file:
-        corpus = file.readlines()
-    return [sentence.lower().split() for sentence in corpus]
+        sentences = file.readlines()
+    return [word_tokenize(sentence.lower()) for sentence in sentences]
 
 
 # General function to save embeddings for both Skip-gram and CBOW models
